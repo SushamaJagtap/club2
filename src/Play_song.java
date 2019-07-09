46431f847b0c66f3713e63e3d15247f8289b8c6c
jalaj22 <catione651@gmail.com>
2019-07-09 15:52:05 +0530

    try to commit

---
+++ src/Play_song.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Play_song {
+
+	public static void main(String[] args) throws InterruptedException {
+	
+		System.setProperty("webdriver.gecko.driver","E:\\JAR\\SeleniumJAR\\geckodriver.exe");
+		FirefoxDriver driver = new FirefoxDriver();
+		
+		driver.get("");
+		
+		
+	}
+
+}
